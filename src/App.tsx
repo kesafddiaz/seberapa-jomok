b1d34abb784057a69d5e8bf828a8a172d742597b
Kesaf Diaz <91782109+kesafddiaz@users.noreply.github.com>
2025-11-19 20:30:19 +0700

    add humam conditional

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
     // Logika perhitungan persentase
     if (currentName) {
         if (currentName === 'humam' || currentName === 'muhammad humam'){
-            calculatedPercent = 100;
+            calculatedPercent = 1000;
         } else {
             calculatedPercent = Math.floor(Math.random() * 101);
         }
@@ -61,7 +61,7 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col'>
+      <div className='flex flex-col max-w-2xl mx-auto'> 
         <h1 className='mb-4'>Seberapa jomok kamu?</h1>
         <h3 className='mb-4'>Masukkan nama kamu di bawah! <span><ArrowDownOutlined /></span></h3>
         <form onSubmit={checkJomok} className='flex flex-col gap-4'>
@@ -76,11 +76,20 @@ function App() {
           </button>
         </form>
 
-        {submittedName && (
+        {submittedName && percent <= 100 && (
           <div className='mt-4 flex flex-col gap-2'>
             <p><b>{submittedName}</b>, Kamu {percent}% Jomok!</p>
             <Progress percent={percent} status='active' showInfo={false} />
             <img src={imgPath} />
+          </div>
+        )}
+        {submittedName && percent == 1000 && (
+          <div className='mt-4 flex flex-col gap-2'>
+            <p className='mb-2'><b>{submittedName}</b>, Kamu {percent}% Jomok!</p>
+            <div className='w-screen relative left-1/2 right-1/2 -mx-[50vw]'>
+              <Progress percent={percent} status='active' showInfo={false} strokeColor='#F54927' />
+            </div>
+            <img src={imgPath} className='w-full' />
           </div>
         )}
       </div>
